06006101fa42d8fd830ed4e3c4ee1c432382bc4a
denis <deniska_pavlov_2002@mail.ru>
2021-11-11 14:19:00 +0300

    Initial commit

---
+++ task.py
@@ -0,0 +1,11 @@
+class Func:
+    def func(self):
+        result____ = []
+        a = 8
+        b = 110
+        
+        result____.append(a + b)
+
+        return result____
+f = Func()
+f.func()
